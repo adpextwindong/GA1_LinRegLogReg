ce1cd2315f02c53e17e6ee44a7b2b3a8cc64cee3
George Crary <craryg@onid.oregonstate.edu>
2018-04-11 12:31:27 -0700

    Add actual prob1.py initial

---
+++ prob1.py
@@ -0,0 +1,15 @@
+import sys
+import datetime
+import numpy as np
+import scipy
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def main():
+	print "main"
+
+if __name__ == "__main__":
+	main()
