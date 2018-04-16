f5d04b8e06f972f15d5473c882b297ce1798cc5e
Danny Barnes <barnesda@oregonstate.edu>
2018-04-15 18:19:21 -0700

    Buggy P2.1 code 👌

--- prob2.py
+++ prob2.py
@@ -19,24 +19,35 @@ def main():
   ### Problem 2.1
   printbold("Problem 2.1")
 
-  #Load data into X and y
+  #Load training data
   X_train = loadX('data/usps-4-9-train.csv', dummy=True)
   y_train = loady('data/usps-4-9-train.csv')
 
+  #Load testing data
+  X_test = loadX('data/usps-4-9-test.csv', dummy=True)
+  y_test = loady('data/usps-4-9-test.csv')
   #Adjust the values of X so x_ij is in [0,1]
   X_train = X_train * (1.0/256)
 
+  #Create a gradient function for the first section
   p2_1grad = lambda pos: logisticgradient(X_train, y_train, pos)
 
+  w_0 = np.matrix([0] * X_train.shape[1]).T
+
   #Calculate w, the optimal weight vector
-  w = gradientdescent(p2_1grad, eta=0.02, epsilon=0.1, dim=X_train.shape[1]) 
+  w = gradientdescent(p2_1grad, eta=0.02, epsilon=0.1, w_0=w_0) 
 
   print("Optimal weight vector, w:")
   print(w)
   print
 
-  X_test = loadX('data/usps-4-9-test.csv', dummy=True)
-  y_test = loady('data/usps-4-9-test.csv')
+  accuracy_train = accuracy(X_train, y_train, w)
+  accuracy_test = accuracy(X_test, y_test, w)
+
+  print("Training accuracy: " + str(accuracy_train))
+  print("Testing accuracy: " + str(accuracy_train))
+  print
+
 
 
 
@@ -45,9 +56,9 @@ def printbold(text):
   print("\033[1m" + text + "\033[0m")
 
 
-def gradientdescent(gradf, eta, epsilon, dim): 
+def gradientdescent(gradf, eta, epsilon, w_0): 
   #Initialize 'w' to a column vector of zeros
-  w = np.matrix([0] * dim).T
+  w = w_0
 
   while True:
     #Calculate the gradient at position 'w'
@@ -71,13 +82,43 @@ def logisticgradient(X, y, w):
     #Grab a row of X and transpose it to a column vector
     x_i = np.matrix(X[i,:]).T
     #Calculate y_hat_i
-    y_hat_i = 1.0/(1 + math.exp((-w.T * x_i)[0,0]))
+    y_hat_i = sigmoid((w.T * x_i)[0,0])
     #Update the gradient
     nabla = nabla + (y_hat_i - y[i,0]) * x_i
 
   return nabla
 
-  
+
+def accuracy(X, y, w):
+  #Store the number of data points
+  total = y.size
+  #Initalize a variable for the number of correct predictions
+  correct = 0
+
+  #Calculate the number of correct predictions
+  for i in range(total):
+    #Grab a row of X and transpose it to a column vector
+    x_i = np.matrix(X[i,:]).T
+    #Calculate the decision term
+    dec = (w.T * x_i)[0,0]
+    if dec > 0 and y[i,0] == 1:
+      correct += 1
+    elif dec < 0 and y[i,0] == 0:
+      correct += 1
+
+  return correct * (1.0/total)
+
+
+def logisticP1(x, w):
+  return sigmoid((w.T * x)[0,0])
+
+
+def logisticP0(x, w):
+  return 1 - logisticP1(x, w)
+
+def sigmoid(val):
+  return 1.0/(1 + math.exp(-val))
+
 
 def loadX(fname, dummy=True):
   """Load independent variable data from a file"""
