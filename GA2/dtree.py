d69ce4fcb0659f16ac90195938ad5492d2c33242
Danny Barnes <barnesda@oregonstate.edu>
2018-04-28 17:28:30 -0700

    Add data structures for decision tree

--- GA2/dtree.py
+++ GA2/dtree.py
@@ -5,11 +5,49 @@ import scipy
 import pandas
 import csv
 import math
+import collections
 
 import matplotlib
 matplotlib.use('Agg')
 #import matplotlib.pyplot as plt
 #from matplotlib.backends.backend_pdf import PdfPages
+
+class Tree(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
+
+Decision = namedtuple('Decision', ['feature', 'threshold'])
+
+def printbold(text):
+	print("\033[1m" + text + "\033[0m")
+
+def applyNormVector(in_vec, norm_vec):
+	out_vec = list(in_vec)
+	for col,(col_min, col_max) in enumerate(norm_vec):
+			col_range = col_max - col_min
+			out_vec[col] = (out_vec[col] -  col_min) / col_range
+
+	return out_vec
+
+### returns a copy with the norm applied
+def applyNormMatrix(in_matrix, norm_vector):
+	ret_matrix = in_matrix.copy()
+	rows = in_matrix.shape[0]
+	assert(in_matrix.shape[1] == len(norm_vector))
+	for row in xrange(rows):
+		for col,(col_min, col_max) in enumerate(norm_vector):
+			col_range = col_max - col_min
+			ret_matrix[row, col] = (ret_matrix[row, col] -  col_min) / col_range
+			
+
+	return ret_matrix
+
+def findNormalizationVector(data_matrix):
+	number_of_features = data_matrix.shape[1]
+	normRanges = [(np.min(data_matrix[:,col]) , (np.max(data_matrix[:,col]))) for col in xrange(number_of_features)]
+	return normRanges
 
 def loadX(fname):
   """Load independent variable data from a file"""
