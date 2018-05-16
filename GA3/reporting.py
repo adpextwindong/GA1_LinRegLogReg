d220e00627e7ea735a034820aa97a5e6b6f0883e
George Crary <craryg@onid.oregonstate.edu>
2018-05-15 19:01:37 -0700

    Add 1.1, 1.2 reporting

--- GA3/reporting.py
+++ GA3/reporting.py
@@ -1,12 +1,13 @@
 import os, glob
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 from collections import namedtuple
 
-CifarData = namedtuple('CifarData', ['Activation','Epochs','Learning_Rate', 'Loss', 'Accuracy'])
+CifarData = namedtuple('CifarData', ['Activation','Epochs','Learning_Rate', 'Loss_Data', 'Accuracy_Data'])
 
 the_data = []
 for fname in glob.glob(os.getcwd() + '/*out.txt'):
@@ -14,11 +15,46 @@ for fname in glob.glob(os.getcwd() + '/*out.txt'):
     details = fname.split('/')
     details.reverse()
     ACTIVATION, EPOCHS, LEARNING_RATE, _ = details[0].strip('out.txt').split('_')
+    EPOCHS = int(EPOCHS)
+    LEARNING_RATE = float(LEARNING_RATE)
+
     with open(fname, 'r') as f:
         lines = f.readlines()
-        LOSS = eval(lines[len(lines) - 2].strip('\n'))
-        ACC = eval(lines[len(lines) - 1].strip('\n'))
-        
-        the_data.append(CifarData(ACTIVATION, EPOCHS, LEARNING_RATE, LOSS, ACC))
+        LOSS_DATA = eval(lines[len(lines) - 2].strip('\n'))
+        ACC_DATA = eval(lines[len(lines) - 1].strip('\n'))
+        assert(EPOCHS == len(LOSS_DATA))
+
+        the_data.append(CifarData(ACTIVATION, EPOCHS, LEARNING_RATE, LOSS_DATA, ACC_DATA))
 
 print the_data
+
+assert len(set([x.Epochs for x in the_data])) == 1
+ROW_INDS = range(1,EPOCHS + 1)
+
+fig = plt.figure()
+ax = plt.subplot(111)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+for d in the_data:
+    d_label = d.Activation + ' ' + str(d.Learning_Rate)
+    plt.plot(ROW_INDS, d.Loss_Data, label=d_label)
+# Put a legend below current axis
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.175),
+          fancybox=True, shadow=True, ncol=3)
+
+plt.show()
+fig.savefig("1_1_Report_Loss.png")
+
+fig = plt.figure()
+ax = plt.subplot(111)
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+for d in the_data:
+    d_label = d.Activation + ' ' + str(d.Learning_Rate)
+    plt.plot(ROW_INDS, d.Accuracy_Data, label=d_label)
+# Put a legend below current axis
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.175),
+          fancybox=True, shadow=True, ncol=3)
+
+plt.show()
+fig.savefig("1_2_Report_Loss.png")
