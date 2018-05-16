70896ec0b7a25a9ab795b2d02ad7d6b39271fe47
George Crary <craryg@onid.oregonstate.edu>
2018-05-15 17:31:03 -0700

    Update test file some more with date handling

--- GA3/test.sh
+++ GA3/test.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
 
-python cifar_p3.py SIG 10 0.1 > sig_10_0.1_out.txt
-python cifar_p3.py SIG 10 0.01 > sig_10_0.01_out.txt
-python cifar_p3.py SIG 10 0.001 > sig_10_0.001_out.txt
-python cifar_p3.py SIG 10 0.0001 > sig_10_0.0001_out.txt
+echo 'Starting Sig testing'
+echo 'SIG 10 0.1'
+echo `date`
+python cifar_p3.py SIG 10 0.1	 > SIG_10_0.1_out.txt
 
-python cifar_p3.py RELU 10 0.1 > RELU_10_0.1_out.txt
-python cifar_p3.py RELU 10 0.01 > RELU_10_0.01_out.txt
-python cifar_p3.py RELU 10 0.001 > RELU_10_0.001_out.txt
+echo 'SIG 10 0.01'
+echo `date`
+python cifar_p3.py SIG 10 0.01	 > SIG_10_0.01_out.txt
+
+echo 'SIG 10 0.001'
+echo `date`
+python cifar_p3.py SIG 10 0.001	 > SIG_10_0.001_out.txt
+
+echo 'SIG 10 0.0001'
+echo `date`
+python cifar_p3.py SIG 10 0.0001 > SIG_10_0.0001_out.txt
+
+echo 'Starting ReLu testing'
+echo 'RELU 10 0.1'
+echo `date`
+python cifar_p3.py RELU 10 0.1    > RELU_10_0.1_out.txt
+
+echo 'RELU 10 0.01'
+echo `date`
+python cifar_p3.py RELU 10 0.01   > RELU_10_0.01_out.txt
+
+echo 'RELU 10 0.001'
+echo `date`
+python cifar_p3.py RELU 10 0.001  > RELU_10_0.001_out.txt
+
+echo 'RELU 10 0.0001'
+echo `date`
 python cifar_p3.py RELU 10 0.0001 > RELU_10_0.0001_out.txt
