175cbc36a4b9e192eb7e6e286d793a3ed665d7e4
George Crary <craryg@onid.oregonstate.edu>
2018-06-11 18:23:34 -0700

    Add initial work on file io

---
+++ FINAL/csv_header_generation.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    csv_header = []
+    for label in ['time','glucose','slope','iob','mob','morning','afternoon','evening','night']:
+        for i in range(7):
+            csv_header.append(
+                't' + str(i) + '_' + label
+            )
+
+    print csv_header
