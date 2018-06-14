404da68149c0aafe81f1362febe05580a7ddc116
Danny Barnes <barnesda@oregonstate.edu>
2018-06-13 23:06:27 -0700

    code

--- FINAL/svm.py
+++ FINAL/svm.py
@@ -1,7 +1,70 @@
-import sklearn as sk
+from sklearn import svm
 import csv
 import pandas as pd
 import numpy as np
+
+def main():
+  data = generate_subsequences(load_data('data/Ind1_train/Subject_2_part1.csv'))
+  
+  k = 5
+
+  spl_idx = []
+ 
+  for i in range(k):
+    l = len(data) * 1.0
+    spl_idx.append([(int)(l * i / k), (int)(l * (i+1) /k)])
+
+  for i in range(k):
+    print("val run : " + str(i+1))
+    st = spl_idx[i][0]
+    end = spl_idx[i][1]
+    X = data.values[:, :-1]
+    y = data.values[:, -1]
+
+    rows_idx = []
+    for i in range(st):
+      rows_idx.append(i)
+    for i in range(len(data)-end):
+      rows_idx.append(i+end)
+
+    # we create 20 points
+    X_train = X[rows_idx,:]
+    y_train = y[rows_idx]
+
+    X_valid = X[st:end,:]
+    y_valid = y[st:end]
+
+    # fit the model
+    clf = svm.SVR()
+    clf.fit(X_train, y_train)
+
+    predictions = clf.predict(X_valid)
+
+    for thr in [x / 100.0 for x in range(10,20)]:
+      res = {'TP': 0, 'FN': 0, 'FP': 0, 'TN': 0}
+      for i in range(len(X_valid)):
+	predict = predictions[i]
+	actual = y_valid[i]
+	#print(predict, actual)
+	if (actual > 0.5):
+	  if (predict > thr):
+	    res['TP'] += 1
+	  else:
+	    res['FN'] += 1
+	else:
+	  if (predict > thr):
+	    res['FP'] += 1
+	  else:
+	    res['TN'] += 1
+      print("Prediction threshold: " + str(thr))
+      print(res)
+      print("F-measure: " + str((2.0*res['TP'])/(2*res['TP']+res['FN']+res['FP'])))
+        
+    
+  
+  #clf_weights.fit(X, y, sample_weight=sample_weight_memes)
+
+
 
 def load_data(fname):
   """Load data from a file"""
@@ -35,9 +98,9 @@ def generate_subsequences(data):
   data['chunk'] = chunkidxs
   #data.set_index('chunk')
 
-  chunks = data.groupby('chunk');
+  chunks = data.groupby('chunk')
   data_filt = chunks.filter(lambda x: len(x) >= 7)
-  newchunks = data_filt.groupby('chunk');
+  newchunks = data_filt.groupby('chunk')
 
   instances = []
   my_list = data_filt['chunk'].values
@@ -83,5 +146,5 @@ def csv_header_test():
 
   return csv_header
 
-print generate_subsequences(load_data('data/Ind1_train/Subject_2_part1.csv'))
-
+if __name__ == "__main__":
+  main()
